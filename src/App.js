aad4d533efe6447ac5009a80b81799baf46e2cab
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-12-19 16:17:03 +0530

    Adding global styles.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <h3>Artistic</h3>
       <p>Dibya Ranjan</p>
     </>
